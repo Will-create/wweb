757ec3c189d8284d8020c3f872105cddfce66332
Louis Bertson <louisbertson@gmail.com>
2025-03-24 08:12:17 +0100

    Removed ignored files from tracking

--- cleanup_instances.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Stopping all containers matching 'instance-*'..."
-docker ps -aq --filter "name=instance-" | xargs -r docker stop
-
-echo "Removing all containers matching 'instance-*'..."
-docker ps -aq --filter "name=instance-" | xargs -r docker rm
-
-echo "Cleanup completed!"
--- index.js
+++
@@ -1,1 +0,0 @@
-require('total4/debug')({ port: process.argv[2] || 3000 });
